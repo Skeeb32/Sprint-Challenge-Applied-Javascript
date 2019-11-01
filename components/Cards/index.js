c9b75a125c8e84e9efc921ac810be89381d82967
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-11-01 10:13:52 -0700

    Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,30 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    // console.log(response)
+  });
+
+function createCard() {
+  const card = document.createElement('div'),
+    headline = document.createElement('div'),
+    author = document.createElement('div'),
+    imageContainer = document.createElement('div'),
+    image = document.createElement('img'),
+    theAuthorName = document.createElement('span');
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imageContainer);
+  author.appendChild(theAuthorName);
+  imageContainer.appendChild(image);
+
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imageContainer.classList.add('img-container');
+
+  return card;
+}
